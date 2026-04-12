5317d3a959b67091ba6413eba406142ee5ac8e60
Kelyn196 <ariasleonela360@gmail.com>
2026-04-12 13:10:54 -0600

    Github pages
    desplegar a github pages

--- vite.config.js
+++ vite.config.js
@@ -20,6 +20,7 @@ function obtenerHtmlFiles() {
 
 export default defineConfig({
     appType: 'mpa',
+    base: process.env.DEPLOY_BASE_URL ?? '/' ,
     build: {
         rollupOptions: {
             input: obtenerHtmlFiles(),
